727a8dc88ed20aade007bf82dcc2a670451cde2a
Chloe-Zhu26 <zhuchloe26@gmail.com>
2025-10-06 23:31:17 -0700

    Create Studies.py

---
+++ pages/Studies.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+st.subheader("Studies on the Wealth Effect")
+st.write("There have been numerous research studies done on the wealth effect, some of which are summarized below:")
+st.markdown("""
+### *Wealth Effects and Macroeconomic Dynamics*
+
+[Read the paper here](https://scholar.harvard.edu/files/kdynan/files/1._wealth_effects_and_macroeconomic_dynamics.pdf)
+
+
+#### Key Insights:
+
+- Strong positive correlation between aggregate consumption and aggregate stock prices
+- MPC out of financial wealth of ~6 cents per dollar in US
+""")
+
+st.markdown("""
+### *Weighing the Wealth Effect*
+
+[Read the paper here](https://usa.visa.com/content/dam/VCOM/global/partner-with-us/documents/weighing-the-wealth-effect.pdf)
+
+
+#### Key Insights:
+
+- Every $1 change in household wealth leads to 4.5 cents in consumer spending change
+- The impact does show a time lag generally from a quarter to a year
+- The wealth effect is larger when asset prices are falling (5 cents per dollar) than when they are rising (3 cents per dollar)
+""")
+
+st.markdown("""
+### *The sudden increase in the wealth effect and its impact on spending*
+
+[Read the paper here](https://usa.visa.com/dam/VCOM/regional/na/us/partner-with-us/economic-insights/documents/vbei-us-economic-insight-wealth-effect.pdf)
+
+
+#### Key Insights:
+
+- The wealth effect has increased in recent years from 9 cents per dollar of wealth (2002-2017) to 34 cents by Q3 2022
+- Increased technology access has increased responsiveness to financial news
+- Stock/bond wealth effects are particularly impactful for discretionary spending
+
+""")
+
+# img = Image.open("C:\Users\chloe\Documents\College Stuff\Code\study3.png")  # Replace with your file path
+# st.image(img, caption="My Local Image", use_column_width=True)
+# - **Impact Estimator Tool**  
+#  Estimate how a change (e.g., a 5% increase) in the NASDAQ might influence future food expenditure.
